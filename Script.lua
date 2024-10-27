3f625feaa4cce2c695c75c708f64736aff2266c3
Axal-Dev <axaldevscript@gmail.com>
2024-10-27 21:34:32 +0100

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,2 +1,2 @@
-
-https://raw.githubusercontent.com/Axal-Dev/Rivals/refs/heads/main/Aimbot.lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Axal-Dev/Rivals/refs/heads/main/Esp.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Axal-Dev/Rivals/refs/heads/main/Aimbot.lua"))()
