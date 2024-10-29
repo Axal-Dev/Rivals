96feb474dfeabc2c6e2c00898a769290f0780efb
Axal-Dev <axaldevscript@gmail.com>
2024-10-29 18:24:40 +0100

    Delete Script.lua

--- Script.lua
+++
@@ -1,2 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Axal-Dev/Rivals/refs/heads/main/Esp.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Axal-Dev/Rivals/refs/heads/main/Aimbot.lua"))()
